704e5bc3d7925c4ed943981fca218c59e8eaa4b1
stage1407 <alexander.stage1407@gmail.com>
2026-07-07 10:59:27 +0200

    Designed the ROOT-API

--- lib/axiom_refiner.ex
+++ lib/axiom_refiner.ex
@@ -5,4 +5,16 @@ defmodule AxiomRefiner do
   The public root API is intentionally not fixed yet.
   Current executable workflows live in the demo modules.
   """
+
+  def refine_files(opts \\ []) do
+    #TODO: Implementation
+  end
+
+  def rank_files(opts \\ []) do
+    #TODO: Implementation
+  end
+
+  def run_isabelle(opts \\ []) do
+    #TODO: Implementation
+  end
 end
