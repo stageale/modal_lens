37af31d6a6481bafac08e4450fdcb1ce7b54c3db
stage1407 <alexander.stage@outlook.de>
2026-07-23 21:16:44 +0200

    Reorganized subroutine

--- graph_ml/subroutine.py
+++ graph_ml/subroutine.py
@@ -1,10 +1,10 @@
 import argparse as ap
 from pathlib import Path
 
-import graph_ml.python.io_schema as io
-import graph_ml.python.features as feat
-import graph_ml.python.clustering as cluster
-import graph_ml.python.mining as mine
+import graph_ml.io_schema as io
+import graph_ml.features as feat
+import graph_ml.clustering as cluster
+import graph_ml.mining as mine
 
 def plot_heatmap(features, feature_names, cluster_labels):
     #TODO: Implement a heat map for visual explanation
